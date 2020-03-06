5c21bbf3a66d697305d2f89048fe2c3b87754334
Sacha Wharton <sacha.wharton@reponsive.co.za>
2020-03-06 15:54:54 +0200

    changes

--- function/ServerlessRealtimeDemo/NegotiateFunction.cs
+++ function/ServerlessRealtimeDemo/NegotiateFunction.cs
@@ -4,18 +4,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Bindings; 
 
 namespace ServerlessRealtimeDemo
 {
     public static class NegotiateFunction
     {
         [FunctionName("negotiate")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")]HttpRequest req, 
-                                        [SignalRConnectionInfo(HubName = "broadcast")]AzureSignalRConnectionInfo info, 
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")]HttpRequest req,
+                                        [SignalRConnectionInfo(HubName = "broadcast")]AzureSignalRConnectionInfo info,
                                         TraceWriter log)
         {
-            log.Info($"Negotiate info for endpoint: {info.Endpoint}");
-
             return info != null
                 ? (ActionResult)new OkObjectResult(info)
                 : new NotFoundObjectResult("Failed to load SignalR Info.");
